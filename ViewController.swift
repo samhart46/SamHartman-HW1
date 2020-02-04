0691011962afaff840737f14db3c7cd8ea82f629
samhart46 <60398615+samhart46@users.noreply.github.com>
2020-02-04 12:55:46 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hartman hw 1
+//
+//  Created by Sam Hartman on 2/4/20.
+//  Copyright © 2020 Sam Hartman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
